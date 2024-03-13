43613bed9c813f4fea378799df60ce420f505097
Guilherme Strabello <gui.strabehonorio@outlook.com>
2024-03-12 21:45:49 -0300

    versão 0.7.0

--- src/components/Clientes.tsx
+++ src/components/Clientes.tsx
@@ -1,19 +1,52 @@
 "use client"
+
 export function Clientes() {
     return (
         <>
-            <h1 className="text-center font-bold text-4xl text-laranja mt-6 mb-6">Nossos Clientes</h1>
-                <div className="overflow-hidden">
-                    <div className="flex whitespace-nowrap animate-slide p-10 space-x-12">
-                        <img src="/images/arita.png" alt="Imagem 1" className="inline-block w-[120px] h-[120px]" />
-                        <img src="/images/sogima.png" alt="Imagem 2" className="inline-block w-[120px] h-[120px]" />
-                        <img src="/images/agrotuba.png" alt="Imagem 3" className="inline-block w-[120px] h-[120px]" />
-                        <img src="/images/taket.png" alt="Imagem 3" className="inline-block w-[120px] h-[120px]" />
-                        <img src="/images/qajuste.png" alt="Imagem 3" className="inline-block w-[120px] h-[120px]" />
-                        <img src="/images/queropraxia.png" alt="Imagem 3" className="inline-block w-[120px] h-[120px]" />
-                        <img src="/images/ligia.png" alt="Imagem 3" className="inline-block w-[120px] h-[120px]" />
+            <h1 className="text-center font-bold text-4xl text-laranja mt-32 mb-24">Nossos Clientes</h1>
+            <div className="container mx-auto py-8">
+                <div className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-x-14">
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/arita.png" alt="Imagem 1" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/sogima.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/agrotuba.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/taket.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/qajuste.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/queropraxia.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/ligia.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/castelo_esfiha.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/draethel.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/ellabelle.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/jackeline.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/hidesushi.png" alt="Imagem 2" />
+                    </div>
+                    <div className="w-full h-64 overflow-hidden">
+                        <img src="/images/leluhkids.png" alt="Imagem 2" />
                     </div>
                 </div>
+            </div>
         </>
     );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,32 +1,55 @@
 "use client"
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+import '/src/app/style.css';
 
 export function HeroSection() {
     return (
         <>
-            <div className="lg:w-80 lg:h-80 md:w-60 md:h-60 bg-laranja rounded-[100%] absolute blur-3xl lg:left-[-6%] lg:top-[10%] md:left-[-20%] md:top-[10%]"></div>
-            <div className="lg:w-80 lg:h-80 md:w-60 md:h-60 bg-roxo rounded-[100%] absolute blur-3xl lg:left-[90%] lg:top-[10%] md:left-[90%] md:top-[10%]"></div>
-                <div className="max-h-96 max-w-[1920px] flex justify-center items-center m-20">
-                    <div className="text-center">
-                        <h1 className="text-laranja xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl mb-4 font-Montserrat">
-                            Busca Constância Na
-                        </h1>
-                        <h1 className="text-laranja flex flex-col items-center xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl font-Montserrat">
-                            Jornada Rumo Ao
-                            <span className="text-roxo bg-laranja p-3 font-Montserrat">
-                                Topo?
-                            </span>
-                        </h1>
-                    </div>
+            <div className="relative">
+      <CSSTransition
+        in={true}
+        appear={true}
+        timeout={1000}
+        classNames="move-up-down"
+        unmountOnExit
+      >
+        <div className="absolute top-36 left-0 w-60 h-60 bg-laranja blur-3xl"></div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={true}
+        appear={true}
+        timeout={1000}
+        classNames="move-up-down"
+        unmountOnExit
+      >
+        <div className="absolute top-36 right-0 w-60 h-60 bg-roxo blur-3xl"></div>
+      </CSSTransition>
+    </div>
+
+            <div className="max-h-96 max-w-[1920px] flex justify-center items-center m-20">
+                <div className="text-center">
+                    <h1 className="text-laranja xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl mb-4 font-Montserrat">
+                        Busca Constância Na
+                    </h1>
+                    <h1 className="text-laranja flex flex-col items-center xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl font-Montserrat">
+                        Jornada Rumo Ao
+                        <span className="text-roxo bg-laranja p-3 font-Montserrat">
+                            Topo?
+                        </span>
+                    </h1>
                 </div>
-                <div className="flex justify-center items-center space-y-4">
-                    <h2 className="font-bold lg:text-4xl text-roxo md:text-2xl sm:text-1xl font-Montserrat-Bold">Conte Conosco para Alcançar Seus Objetivos!</h2>
-                </div>
-                <div className="flex justify-center items-center space-y-4">
-                    <button className="lg:p-3 bg-laranja m-10 lg:text-white md:text-white font-bold rounded-lg hover:bg-roxo duration-300 font-NeueMontreal-Bold antialiased">Conheça nosso trabalho</button>
-                </div>
-                <div>
-                    <img src="/Ilustração.png" alt="Ilustração" className="m-auto w-56 h-56 mt-3" />
-                </div>
+            </div>
+            <div className="flex justify-center items-center space-y-4">
+                <h2 className="font-bold lg:text-4xl text-roxo md:text-2xl sm:text-1xl font-Montserrat-Bold">Conte Conosco para Alcançar Seus Objetivos!</h2>
+            </div>
+            <div className="flex justify-center items-center space-y-4">
+                <button className="lg:p-3 bg-laranja m-10 lg:text-white md:text-white font-bold rounded-lg hover:bg-roxo duration-300 font-NeueMontreal-Bold antialiased">Conheça nosso trabalho</button>
+            </div>
+            <div>
+                <img src="/Ilustração.png" alt="Ilustração" className="m-auto w-56 h-56 mt-3" />
+            </div>
         </>
     )
 }
