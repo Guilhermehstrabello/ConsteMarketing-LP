dde224af3c9463c5a80278758e5c74e792f69bf7
Guilherme Strabello <gui.strabehonorio@outlook.com>
2024-03-19 17:55:11 -0300

    versão 0.7.9

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,27 +6,9 @@ export function HeroSection() {
     return (
         <>
             <div className="relative">
-                <CSSTransition
-                    in={true}
-                    appear={true}
-                    timeout={1000}
-                    classNames="move-up-down"
-                    unmountOnExit
-                >
-                    <div className="absolute top-36 left-0 w-60 h-60 bg-laranja blur-3xl"></div>
-                </CSSTransition>
-
-                <CSSTransition
-                    in={true}
-                    appear={true}
-                    timeout={1000}
-                    classNames="move-up-down"
-                    unmountOnExit
-                >
-                    <div className="absolute top-36 right-0 w-60 h-60 bg-roxo blur-3xl"></div>
-                </CSSTransition>
+                <div className="absolute top-40 left-0 sm:left-0 md:left-0 lg:w-60 sm:w-30 h-1/4 sm:h-40 bg-laranja blur-3xl"></div>
+                <div className="absolute top-40 right-0 sm:right-0 md:right-0 lg:w-60 sm:w-30 h-1/4 sm:h-40 bg-roxo blur-3xl"></div>
             </div>
-
             <div className="max-h-96 max-w-[1920px] flex justify-center items-center m-20">
                 <div className="text-center">
                     <h1 className="text-laranja xl:text-6xl lg:text-3xl md:text-2xl sm:text-xl mb-4 font-Montserrat">
@@ -44,7 +26,7 @@ export function HeroSection() {
                 <h2 className="font-bold text-roxo lg:text-3xl  md:text-2xl sm:text-xl font-Montserrat-Bold">Conte Conosco para Alcançar Seus Objetivos!</h2>
             </div>
             <div className="flex justify-center items-center space-y-4">
-                <button className="lg:p-3 bg-laranja m-10 lg:text-white md:text-white font-bold rounded-lg hover:bg-roxo duration-300 font-NeueMontreal-Bold antialiased">Conheça nosso trabalho</button>
+                <button className="md:p-3 lg:p-4 bg-laranja m-10 lg:text-white md:text-white font-bold rounded-lg hover:bg-roxo duration-300 font-NeueMontreal-Bold antialiased">Conheça nosso trabalho</button>
             </div>
             <div>
                 <img src="/images/Ilustração.png" alt="Ilustração" className="m-auto w-56 h-56 mt-3" />
